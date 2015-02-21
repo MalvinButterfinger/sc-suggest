3b523c78f11b32b9b62ca27b924d28d99ae5bd77
Malvin Butterfinger <malvin.butterfinger@gmail.com>
2015-02-21 01:15:40 +0100

    Redesign, SC-Q-Promises

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,110 @@
  * Controller of the scSuggestApp
  */
 angular.module('scSuggestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $timeout) {
+      var ctrl = this;
 
-    
+      $scope.connect = connect;
+      $scope.getTracks = getTracks;
+      $scope.next = next;
+      $scope.connected = false;
+      $scope.hasResults = false;
+      $scope.show = 'tracks';
+      $scope.currentPosition = 0;
+      $scope.currentItem = {};
+      $scope.playTrack = playTrack;
+      $scope.showTracks = function () { return $scope.show == 'tracks'; }
+      init();
+
+      $scope.$watch('hasResults', onHasResults);
+      $scope.safeApply = function (fn) {
+          var phase = this.$root.$$phase;
+          if (phase == '$apply' || phase == '$digest') {
+              if (fn && (typeof (fn) === 'function')) {
+                  fn();
+              }
+          } else {
+              this.$apply(fn);
+          }
+      };
+
+      function init() {
+          SC.initialize({
+              client_id: '747e72b7a1a64556d542a254e330fcc0',
+              //redirect_uri: 'http://MalvinButterfinger.github.io/sc-suggest/callback.html'
+              redirect_uri: 'http://localhost:9000/callback.html'
+          });
+      }
+
+      function connect() {
+          qsc.connect().then(qsc.getMyData).then(function (myData) {
+              $scope.connected = true;
+              $scope.myTracks = myData.favorites;
+              $scope.myFollowings = myData.followings;
+          });
+      }
+
+      function getTracks() {
+          $scope.hasResults = false;
+          $timeout(function () {
+              qsc.getFavoriterFavoritesForUserFavorites('/me').then(function (tracks) {
+                  tracks.sort(function (a, b) { return b.count - a.count });
+                  $scope.hasResults = true;
+                  $scope.suggested = tracks.filter(function (t) {
+                      return $scope.myTracks.filter(function (tt) { return tt.id == t.id; }).length == 0 && $scope.myFollowings.filter(function (ff) { return ff.id == t.item.user.id; }).length == 0;
+                  }).map(function (t, i) {
+                      return {
+                          rank: i,
+                          status: '',
+                          info: t.item.user.username + ' - ' + t.item.title,
+                          purl: t.item.permalink_url,
+                          url: 'https://w.soundcloud.com/player/?url=' + t.item.permalink_url,
+                          count: t.count,
+                          id: t.id
+                      }
+                  });;
+                  $scope.$apply();
+              });
+          });
+      }
+
+      function onHasResults(nv, ov) {
+          if (!nv) return;
+          $timeout(function () {
+              angular.element('#widget-iframe').attr('src', $scope.suggested[0].url);
+              ctrl.widget = ctrl.widget || SC.Widget('widget-iframe');
+              playCurrent();
+          });
+      }
+
+      function play() {
+          if ($scope.hasResults) {
+              $scope.currentPosition = 0;
+              playCurrent();
+          }
+      }
+
+      function playTrack(pos) {
+          $scope.currentItem.status = '';
+          $scope.currentPosition = pos;
+          playCurrent();
+      }
+
+      function next() {
+          if ($scope.hasResults) {
+              $scope.currentItem.status = '';
+              $scope.currentPosition++;
+              if ($scope.currentPosition == $scope.suggested.length) $scope.currentPosition = 0;
+              playCurrent();
+          }
+      }
+
+      function playCurrent() {
+          $scope.currentItem = $scope.suggested[$scope.currentPosition];
+          $scope.currentItem.status = 'playing';
+          ctrl.widget.load($scope.currentItem.purl, {auto_play: true});
+          ctrl.widget.bind(SC.Widget.Events.FINISH, function (data) {
+              if (data.relativePosition == 1) next();
+          });
+      }
   });
---
+++ app/scripts/qsc.js
@@ -0,0 +1,128 @@
+﻿(function (root) {
+    root.qsc = factory();
+    function factory() {
+        var qsc = {
+            get: get,
+            getMyData: getMyData,
+            getFavorites: getFavorites,
+            getFavoriters: getFavoriters,
+            getFollowings: getFollowings,
+            getFavoritesForTrackFavoriters: getFavoritesForTrackFavoriters,
+            getFavoriterFavoritesForUserFavorites: getFavoriterFavoritesForUserFavorites,
+            connect: connect
+        };
+        return qsc;
+
+        function getMyData() {
+            var deferred = Q.defer();
+            getFavorites('/me').then(function (favorites) {
+                getFollowings('/me').then(function (followings) {
+                    deferred.resolve({ favorites: favorites, followings: followings });
+                });
+            });
+            return deferred.promise;
+        }
+
+        function getFavoriterFavoritesForUserFavorites(userUrl) {
+            var deferred = Q.defer();
+            getFavorites(userUrl).then(getFavoritesForTracksFavoriters).then(deferred.resolve, deferred.reject);
+            return deferred.promise;
+        }
+
+        function getFavoritesForTrackFavoriters(trackUrl) {
+            var deferred = Q.defer();
+            getFavoriters(trackUrl).then(getFavoritesForUsers).then(deferred.resolve);
+            rejectAfter(5000, 500, deferred);
+            return deferred.promise;
+        }
+
+        function getFavoritesForUsers(users) {
+            return groupItemsForItems(users, function(u) { return getFavorites('/users/' + u.id); });
+        }
+
+        function getFollowingsForUsers(users) {
+            return groupItemsForItems(users, function(u) { return getFollowings('/users/' + u.id); });
+        }
+
+        function getFollowersForUsers(users) {
+            return groupItemsForItems(users, function(u) { return getFollowers('/users/' + u.id); });
+        }
+
+        function getFavoritersForTracks(tracks) {
+            return groupItemsForItems(tracks, function(t) { return getFavoriters('/tracks/' + t.id); });
+        }
+
+        function getFavoritesForTracksFavoriters(tracks) {
+            return groupItemsForItems(tracks, function (t) { return getFavoriters('/tracks/' + t.id); }).then(getFavoritesForUsers);
+        }
+
+
+        function groupItemsForItems(items, getItems) {
+            var deferred = Q.defer();
+            Q.all(items.filter(function(x){return (!x.count) || (x.count > 1);}).map(getItems)).then(function (its) {
+                var grouped = [];
+                for (var i = 0; i < its.length; i++) {
+                    var itg = its[i];
+                    for (var j = 0; j < itg.length; j++) {
+                        var tr = itg[j];
+                        var it = grouped.filter(function (i) { return i.item.id == tr.id; });
+                        if (it.length == 0) {
+                            grouped.push({ id: tr.id, item: tr, count: 1 });
+                        }
+                        else {
+                            it[0].count++;
+                        }
+                    }
+                }
+                deferred.resolve(grouped);
+            }, deferred.reject);
+            return deferred.promise;
+        }
+
+        function getFavorites(userUrl) {
+            return get(userUrl + '/favorites');
+        }
+
+        function getFavoriters(trackUrl) {
+            return get(trackUrl + '/favoriters');
+        }
+
+        function getFollowers(userUrl) {
+            return get(userUrl + '/followers');
+        }
+
+        function getFollowings(userUrl) {
+            return get(userUrl + '/followings');
+        }
+
+        function connect() {
+            var deferred = Q.defer();
+            SC.connect(function () {
+                deferred.resolve();
+            });
+            rejectAfter(5000, 500, deferred, connect);
+            return deferred.promise;
+        }
+
+        function get(url) {
+            var deferred = Q.defer();
+            SC.get(url, function(data) {
+                deferred.resolve(data);
+            });
+            rejectAfter(5000, 500, deferred, url);
+            return deferred.promise;
+        }
+
+        function rejectAfter(timeout, step, deferred, msg) {
+            var t = 0;
+            (function waitOrReject() {
+                if (t > timeout / step) {
+                    deferred.reject('Timeout: ' + msg)
+                    return;
+                };
+                t++;
+                setTimeout(waitOrReject, step);
+            })();
+        }
+    }
+})(window);
