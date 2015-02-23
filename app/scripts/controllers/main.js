91c458a57c97ccc975ef47465e1e0bb3142cda7c
Malvin Butterfinger <malvin.butterfinger@gmail.com>
2015-02-23 21:16:45 +0100

    :o

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -67,8 +67,8 @@ angular.module('scSuggestApp').controller('MainCtrl', function ($scope, $timeout
 
     function init() {
         SC.initialize({
-            client_id: '747e72b7a1a64556d542a254e330fcc0',
-            redirect_uri: 'http://MalvinButterfinger.github.io/sc-suggest/callback.html'
+            client_id: '9fb49cbf420185e08526de7349f29b97',
+            redirect_uri: 'http://localhost:9000/callback.html'
         });
     }
 
